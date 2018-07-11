55a131c1e2e4ddd74ed3d1220076e7f7398b51e3
kevin.mcgregor@mail.mcgill.ca <kevin.mcgregor@d1p-hydratm01.ldi.lan>
2018-07-11 14:39:51 -0400

    init

---
+++ R/mdine-package.R
@@ -0,0 +1,17 @@
+#' The 'mdine' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name mdine-package
+#' @aliases mdine
+#' @useDynLib mdine, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+#' 
+NULL
