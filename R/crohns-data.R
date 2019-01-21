7aabed0f2343ae8ffbd98afa6a8042c9240c4fd6
kevin.mcgregor@mail.mcgill.ca <kevin.mcgregor@d1p-hydratm01.ldi.lan>
2019-01-21 16:47:33 -0500

    added dataset

---
+++ R/crohns-data.R
@@ -0,0 +1,22 @@
+#' Microbiome family-level data from Crohn's and control samples
+#'
+#' Counts from 16S sequencing data on treatment-naive subjects with Crohn's disease
+#' or controls.  Other covariates available include age, sex, and antibiotic use.
+#' 
+#' @docType data
+#'
+#' @usage data(crohns)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Gevers et al. (2014) Cell Host & Microbe 15.3 (2014): 382-392.
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/24629344}{PubMed})
+#'
+#' @source \href{https://qiita.ucsd.edu/study/description/1939}{Qiita database}
+#'
+#' @examples
+#' data(grav)
+#' 
+"crohns"
