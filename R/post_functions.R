d07f5fd082defb61ede5bfbc45330743035be702
kevin.mcgregor@mail.mcgill.ca <kevin.mcgregor@d1p-hydratm01.ldi.lan>
2019-02-11 14:27:46 -0500

    exporting ci2adj function

--- R/post_functions.R
+++ R/post_functions.R
@@ -2,18 +2,20 @@
 #' Get adjacency matrix for estimated networks based on credible intervals
 #'
 #' @param obj An object of 'mdine' class
-#' @param weighted Logical: should
+#' @param weighted Logical: should the adjacency matrices be weighted?
 #'
-#' @return Adjacency matrices for
+#' @return A list containing adjacency matrices for the two estimated precision matrices
 #' @export
 #'
-#' @examples
-ci_to_adj <- function(obj, weighted=FALSE) {
-  # if (class(obj) != "mdine") stop("obj must be of class \"mdine\"")
-  # upper0 <-
-  # adj <- (lower>0) | (upper<0)
-  # diag(adj) <- 0
-  # return(adj)
-  return(1)
+#' @examples ls()
+ci2adj <- function(obj, weighted=FALSE) {
+  if (class(obj) != "mdine") stop("obj must be of class \"mdine\"")
+
+  adj0 <- (obj$ci$invsigma0[[1]]>0) | (obj$ci$invsigma0[[2]]<0)
+  adj1 <- (obj$ci$invsigma1[[1]]>0) | (obj$ci$invsigma1[[2]]<0)
+  diag(adj0) <- 0
+  diag(adj1) <- 0
+
+  return(list(adj0=adj0, adj1=adj1))
 }
 
