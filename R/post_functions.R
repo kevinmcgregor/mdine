9f3f5e4e4f3c350a771ea7bb624e3dc200743cd8
kevin.mcgregor@mail.mcgill.ca <kevin.mcgregor@d1p-hydratm01.ldi.lan>
2019-02-12 11:24:00 -0500

    created some igraph functionality

--- R/post_functions.R
+++ R/post_functions.R
@@ -33,16 +33,63 @@ ci2adj <- function(obj, weighted=FALSE) {
 #' @return A matrix with a 1 if that element of the precision matrix differs significantly with respect to the binary covariate
 #' @export
 #'
+#'
 #' @examples ls()
 sig_diff_prec <- function(obj) {
   if (class(obj) != "mdine") stop("obj must be of class \"mdine\"")
 
   is.sig <- (obj$ci$invsigma_diff[[1]]>0) | (obj$ci$invsigma_diff[[2]]<0)
 
-  return(is.sig)
+  return(1*is.sig)
 }
 
 
+#' Plot a network for each group
+#'
+#' @param obj An object of class \code{mdine}
+#'
+#' @return No return value
+#' @export
+#'
+#'
+#' @examples ls()
+plot_networks <- function(obj) {
+  if (class(obj) != "mdine") stop("obj must be of class \"mdine\"")
+  return(1)
+}
+
+
+#' Create an \code{igraph} object from weighted adjacency matrix
+#'
+#' @param w.adj The weighted adjacency matrix to be converted to an \code{igraph} object
+#' @param v.col The vertex colours.  Numeric vector of length equal to number of OTUs in network
+#' @param e.col The edge colour.  If NULL, then colour green/red based on positive/negative weights.
+#'
+#' @return An object of class \code{igraph} corresponding to the provided adjacency matrix
+#' @export
+#'
+#' @importFrom igraph categorical_pal graph.adjacency
+#'
+#' @examples ls()
+adj2ig <- function(w.adj=NULL, v.col=NULL, e.col=NULL) {
+  J <- NCOL(w.adj)
+
+  if (is.null(col)) {
+    palette <- igraph::categorical_pal(J)
+  } else {
+    palette <- col
+  }
+
+  graph = graph.adjacency(w.adj, mode="undirected", weighted=TRUE, diag = FALSE)
+  if (is.null(e.col)) {
+    E(graph)$color[E(graph)$weight>0] <- "forestgreen"
+    E(graph)$color[E(graph)$weight<=0] <- "orangered"
+  }
+
+  V(graph)$color <- palette
+
+  return(graph)
+}
 
 
 
